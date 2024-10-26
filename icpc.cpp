5e5d39b8d27958f2074fb9f4f7ad79c564c04944
ChengxiX <Hanson.xcx@outlook.com>
2024-10-26 19:29:36 +0800

    每次选最后

--- icpc.cpp
+++ icpc.cpp
@@ -241,7 +241,6 @@ void Scroll() {
                     else {
                         Scores[*s_it][j].freezed = 0;
                         Scores[*s_it][j].failed_b4_freezed = Scores[*s_it][j].failed_b4_ac;
-                        break;
                     }
                 }
             }
